d3f4bb74fc0f8e710996e478343eab53ef8a5890
UnskilledFreak <git@unskilledfreak.de>
2020-09-25 22:21:24 +0200

    oops

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SongDataCleaner")]
-[assembly: AssemblyDescription("Cleaning mod for custom dong data")]
+[assembly: AssemblyDescription("Cleaning mod for custom song data")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SongDataCleaner")]
